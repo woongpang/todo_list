856c864ce9dd8fa25861c38d696cfa664be7e279
woongpang <jr.woong@gmail.com>
2023-05-01 14:12:15 +0900

    page_size 수정

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -169,3 +169,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+PAGE_SIZE = 3
